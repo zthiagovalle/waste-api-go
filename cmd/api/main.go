78e3ffcab4babc9a6da8ad02f9fa39bb2327f0be
Thiago Valle <zthiagovalle@gmail.com>
2023-03-24 16:01:40 -0300

    enable cors

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	setupRoutes(app)
 
